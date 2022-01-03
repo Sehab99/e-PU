b661b45be44f18ab55837ca953213fa0a6327d2f
Sehab <sehabatmain@gmail.com>
2022-01-04 00:45:32 +0530

    Final

--- course/urls.py
+++ course/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register, name="register"),
     path('courses/', views.courses, name="courses"),
     path('topics/<str:courseid>/', views.topics, name="topics"),
     path('topics/<str:courseid>/<str:topicid>/', views.video, name="video"),
--- course/views.py
+++ course/views.py
@@ -1,13 +1,50 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
 from .forms import *
-from django.shortcuts import render
 from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('courses')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('courses/')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+        context = {}
+        return render(request, 'index.html', context)
 
 
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('courses')
+    else:
+        if request.method == 'POST':
+            form = Registration(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('index')
+        else:
+            form = Registration()
+        context = {'form': form}
+        return render(request, 'register.html', context)
+
+
+@login_required(login_url='index')
 def courses(request):
     courses = Courses.objects.all().order_by('-id')
     form = AddCourse()
@@ -21,6 +58,7 @@ def courses(request):
     return render(request, 'courses.html', context)
 
 
+@login_required(login_url='index')
 def topics(request, courseid):
     courses = Courses.objects.get(id=courseid)
     topics = courses.topics_set.all()
@@ -40,6 +78,7 @@ def topics(request, courseid):
     return render(request, 'topics.html', context)
 
 
+@login_required(login_url='index')
 def video(request, courseid, topicid):
     courses = Courses.objects.get(id=courseid)
     topics = courses.topics_set.all()
